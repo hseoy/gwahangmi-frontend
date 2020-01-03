cc8367b03a1fbedd23ebd46f098e0f4741723517
Yunseo Hwang <yunseo.h68@gmail.com>
2020-01-03 23:11:26 +0900

    Feat : Main Home에 인기글 Slider 추가

--- src/components/Posts/PopularPosts.vue
+++ src/components/Posts/PopularPosts.vue
@@ -1,7 +1,107 @@
 <template>
   <div id="PopularPosts">
-    <h2 class="title">인기글</h2>
-    <post-thumbnail />
+    <h2 class="title-wrap">
+      <span class="title">
+        인기글
+      </span>
+      <span>
+        |
+      </span>
+      <span class="sub-title">
+        과학미 뿜뿜, 과학의 아름다움을 알린 오늘의 글
+      </span>
+    </h2>
+
+    <div class="post-wrap">
+      <div class="post-content">
+        <div class="scene">
+          <div class="cube" v-bind:class="getShowCubeSide">
+            <div class="cube-face cube-face-left">
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title1"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title2"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title3"
+              />
+            </div>
+            <div class="cube-face cube-face-front">
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title4"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title5"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title6"
+              />
+            </div>
+            <div class="cube-face cube-face-right">
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title7"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title8"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title9"
+              />
+            </div>
+            <div class="cube-face cube-face-back">
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title10"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title11"
+              />
+              <post-thumbnail
+                thumbnailImg="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+                title="title12"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <a class="prev" v-on:click="minusSlides;">&#10094;</a>
+      <a class="next" v-on:click="plusSlides;">&#10095;</a>
+    </div>
+
+    <div class="post-dots">
+      <span
+        class="dot"
+        v-bind:class="{ 'dot-active': cubeSides.left }"
+        v-on:click="currentSide(0)"
+      ></span>
+      <span
+        class="dot"
+        v-bind:class="{ 'dot-active': cubeSides.front }"
+        v-on:click="currentSide(1)"
+      ></span>
+      <span
+        class="dot"
+        v-bind:class="{ 'dot-active': cubeSides.right }"
+        v-on:click="currentSide(2)"
+      ></span>
+      <span
+        class="dot"
+        v-bind:class="{ 'dot-active': cubeSides.back }"
+        v-on:click="currentSide(3)"
+      ></span>
+    </div>
   </div>
 </template>
 
@@ -12,18 +112,107 @@ export default {
   name: "PopularPosts",
   components: {
     "post-thumbnail": PostThumbnail
+  },
+  data() {
+    return {
+      sideIndex: 0,
+      cubeSides: {
+        left: true,
+        front: false,
+        right: false,
+        back: false
+      },
+      cubeSidesName: ["left", "front", "right", "back"],
+      autoSlide: true,
+      playSlide: null,
+      startSlides: null,
+      stopSlides: null
+    };
+  },
+  mounted() {
+    this.autoSlides();
+  },
+  computed: {
+    getShowCubeSide() {
+      return {
+        "show-front":
+          this.cubeSides.front &&
+          !this.cubeSides.right &&
+          !this.cubeSides.back &&
+          !this.cubeSides.left,
+        "show-right":
+          !this.cubeSides.front &&
+          this.cubeSides.right &&
+          !this.cubeSides.back &&
+          !this.cubeSides.left,
+        "show-back":
+          !this.cubeSides.front &&
+          !this.cubeSides.right &&
+          this.cubeSides.back &&
+          !this.cubeSides.left,
+        "show-left":
+          !this.cubeSides.front &&
+          !this.cubeSides.right &&
+          !this.cubeSides.back &&
+          this.cubeSides.left
+      };
+    }
+  },
+  methods: {
+    autoSlides() {
+      this.startSlides = function() {
+        this.playSlide = setInterval(this.plusSlides, 3000);
+      };
+      this.stopSlides = function() {
+        clearInterval(this.playSlide);
+      };
+      if (this.autoSlide) {
+        this.startSlides();
+      } else {
+        this.stopSLides();
+      }
+    },
+    plusSlides() {
+      this.showSides((this.sideIndex += 1));
+    },
+    minusSlides() {
+      this.showSides((this.sideIndex += -1));
+    },
+    currentSide(n) {
+      this.showSides((this.sideIndex = n));
+    },
+    showSides(n) {
+      if (n == this.cubeSidesName.length) {
+        this.sideIndex = 0;
+      }
+      if (n < 0) {
+        this.sideIndex = this.cubeSidesName.length - 1;
+      }
+
+      this.cubeSides.left = false;
+      this.cubeSides.front = false;
+      this.cubeSides.right = false;
+      this.cubeSides.back = false;
+
+      switch (this.sideIndex) {
+        case 0:
+          this.cubeSides.left = true;
+          break;
+        case 1:
+          this.cubeSides.front = true;
+          break;
+        case 2:
+          this.cubeSides.right = true;
+          break;
+        case 3:
+          this.cubeSides.back = true;
+          break;
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss">
-#PopularPosts {
-  display: block;
-  background-color: red;
-
-  .title {
-    font-size: 35px;
-  }
-}
 @import "@/styles/gwahangmi/post/popular-posts.scss";
 </style>
--- src/components/Posts/PostThumbnail.vue
+++ src/components/Posts/PostThumbnail.vue
@@ -1,20 +1,45 @@
 <template>
-  <div class="PostThumbnail">
+  <div class="post-thumbnail">
     <div class="thumbnail-wrap">
-      <img
-        class="thumbnail-img"
-        src="{ thumbnail_img }"
-        alt="thumbnail image"
-      />
-      <h3 class="thumbnail-date">2020.01.01</h3>
-      <h3 class="thumbnail-title">title</h3>
+      <div class="thumbnail-img">
+        <img :src="thumbnailImg" alt="thumbnail image" />
+      </div>
+      <div class="thumbnail-info-wrap">
+        <div class="thumbnail-info">
+          <p class="thumbnail-date">2020.01.01</p>
+          <h3 class="thumbnail-title">{{ title }}</h3>
+        </div>
+      </div>
     </div>
     <h3 class="thumbnail-author">
       <span class="author-name">Author</span>
-      <span class="author-rank">Rank</span>
+      <div class="author-rank" :title="rank[0].title">
+        <img :src="rank[0].img" alt="rank image" />
+      </div>
     </h3>
   </div>
 </template>
+
+<script>
+export default {
+  name: "post-thumbnail",
+  props: {
+    postID: String,
+    title: String,
+    thumbnailImg: String
+  },
+  data() {
+    return {
+      rank: [
+        {
+          title: "RANK_1:과",
+          img: require("@/assets/rank_1.png")
+        }
+      ]
+    };
+  }
+};
+</script>
 
 <style lang="scss">
 @import "@/styles/gwahangmi/post/post-thumbnail.scss";
