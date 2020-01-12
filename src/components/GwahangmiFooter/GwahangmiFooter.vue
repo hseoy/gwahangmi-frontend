9e03848880cc8b58365605440538205e8289556f
Yunseo Hwang <yunseo.h68@gmail.com>
2020-01-13 01:21:23 +0900

    Feat : Footer UI 추가

--- src/components/GwahangmiFooter/GwahangmiFooter.vue
+++ src/components/GwahangmiFooter/GwahangmiFooter.vue
@@ -1,5 +1,28 @@
 <template>
   <div id="gwahangmiFooter">
-    <p>Copyright</p>
+    <div class="outter">
+      <div class="inner">
+        <p>
+          Copyright 2020.
+          <a
+            class="author"
+            target="_blank"
+            rel="noopener"
+            href="https://github.com/yunseo-h68"
+            >Yunseo Hwang</a
+          >. All rights reserved.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "GwahangmiFooter"
+};
+</script>
+
+<style lang="scss">
+@import "@/styles/gwahangmi/footer/footer.scss";
+</style>
