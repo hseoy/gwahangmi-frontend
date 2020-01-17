3034fe69969abc1e83f642eb062e9557d107e0d8
Yunseo Hwang <yunseo.h68@gmail.com>
2020-01-17 22:45:19 +0900

    Refactor : Scss 파일 분할 및 글 Upload 시 Editor 내용 초기화하도록 수정

--- src/components/Posts/MdEditor.vue
+++ src/components/Posts/MdEditor.vue
@@ -6,7 +6,10 @@
       v-bind:value="mdText"
       v-on:input="updateValue($event.target.value)"
     ></textarea>
-    <div class="md-viewer-cont preview" v-html="previewText"></div>
+    <div
+      class="md-viewer-cont preview md-post-content--wrap"
+      v-html="previewText"
+    ></div>
   </div>
 </template>
 
@@ -15,7 +18,6 @@ const marked = require("marked");
 
 export default {
   name: "md-editor",
-  props: {},
   data() {
     return {
       mdText: "# Hello, 과학미"
@@ -30,152 +32,15 @@ export default {
     updateValue(value) {
       this.mdText = value;
       this.$emit("input", this.previewText);
+    },
+    initValue() {
+      this.mdText = "# Hello, 과학미";
     }
   }
 };
 </script>
 
 <style lang="scss">
-.md-editor {
-  height: calc(100% - 272px);
-  position: relative;
-  padding: 0 10px;
-
-  .md-viewer-cont {
-    width: 50%;
-    height: 100%;
-    border: 2px solid $color-accent;
-
-    &.input {
-      float: left;
-      resize: none;
-      padding: 20px;
-      border-right: none;
-    }
-
-    &.preview {
-      margin-left: 50%;
-      width: 50%;
-      height: 100%;
-      padding: 20px;
-      overflow: scroll;
-
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6 {
-        font-family: $font-accent;
-        line-height: 1.25;
-        margin-bottom: 16px;
-        margin-top: 24px;
-        padding-bottom: 0.3em;
-      }
-
-      h1,
-      h2 {
-        border-bottom: 1px solid #eaecef;
-      }
-
-      a {
-        position: relative;
-        color: $color-accent;
-        text-decoration: none;
-
-        &[href] {
-          box-shadow: inset 0 -4px 0 $color-primary;
-          transition: box-shadow 0.2s, color 0.2s;
-        }
-
-        &:hover {
-          box-shadow: inset 0 -20px 0 $color-primary;
-          color: $color-accent;
-        }
-      }
-
-      ol,
-      ul {
-        display: block;
-
-        li {
-          display: list-item;
-
-          li {
-            margin-left: 40px;
-          }
-        }
-      }
-
-      input,
-      textarea,
-      select,
-      button {
-        font-family: $font-base;
-      }
-
-      img {
-        max-width: 100%;
-        vertical-align: middle;
-        border: 0 none;
-      }
-
-      button {
-        cursor: pointer;
-        vertical-align: middle;
-      }
-
-      input,
-      textarea {
-        color: $color-accent;
-        border-radius: 5px;
-        padding: 15px;
-        border: 1px solid #cfcfcf;
-      }
-
-      input {
-        vertical-align: middle;
-      }
-
-      input::placeholder,
-      textarea::placeholder {
-        color: darken($color-accent, 60%);
-      }
-
-      blockquote {
-        color: lighten($color-accent, 60%);
-        border-left: 4px solid lighten($color-accent, 60%);
-        padding-left: 20px;
-        letter-spacing: -1px;
-        font-style: italic;
-
-        > :last-child {
-          margin-bottom: 0;
-        }
-      }
-
-      pre,
-      code {
-        font-family: Courier, $font-base;
-        border-radius: 4px;
-        background-color: #f6f8fa;
-      }
-
-      code {
-        padding: 1px 5px;
-      }
-
-      pre {
-        padding: 8px 12px;
-        overflow-x: auto;
-
-        > code {
-          border: 0;
-          padding-right: 0;
-          padding-left: 0;
-        }
-      }
-    }
-  }
-}
+@import "@/styles/gwahangmi/post/post-content.scss";
+@import "@/styles/gwahangmi/post/md-editor.scss";
 </style>
--- src/components/Posts/PostEditor.vue
+++ src/components/Posts/PostEditor.vue
@@ -13,7 +13,7 @@
             v-model="post.category"
             require
             name="postCategory"
-            id="postCategory"
+            class="postCategory"
           >
             <option value="" disabled selected>카테고리 선택</option>
             <option value="physics">카테고리:물리학</option>
@@ -27,12 +27,12 @@
       <div class="post-title-input-wrap">
         <input class="post-title-input" type="text" v-model="post.title" />
       </div>
-      <md-editor v-model="post.content"></md-editor>
+      <md-editor ref="mdEditor" v-model="post.content"></md-editor>
 
       <div class="post-upload-button-wrap">
         <div class="outter">
           <div class="inner">
-            <button class="post-upload-button">
+            <button class="post-upload-button" v-on:click="postUpload">
               제출
             </button>
           </div>
@@ -60,7 +60,8 @@ export default {
         category: "",
         title: "Hello, 과학미",
         content: "<h1>Hello, 과학미</h1>"
-      }
+      },
+      init: false
     };
   },
   computed: {
@@ -74,131 +75,21 @@ export default {
   methods: {
     closeEditor() {
       this.$emit("close-editor");
+    },
+    postUpload() {
+      this.postDataInit();
+      this.closeEditor();
+    },
+    postDataInit() {
+      this.$refs.mdEditor.initValue();
+      this.post.category = "";
+      this.post.title = "Hello, 과학미";
+      this.post.content = "<h1>Hello, 과학미</h1>";
     }
   }
 };
 </script>
 
 <style lang="scss">
-.editor-wrap-wrap {
-  .detail-background {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    overflow: hidden;
-    width: 100%;
-    height: 0%;
-    background-color: rgba(0, 0, 0, 0.7);
-    transition: 0.3s;
-  }
-  .editor-wrap {
-    position: fixed;
-    display: none;
-    top: 50%;
-    left: 50%;
-    width: 80vw;
-    height: 80vh;
-    background-color: $color-white;
-    border: 3px solid $color-accent;
-    border-radius: 50px;
-    margin-top: -40vh;
-    margin-left: -40vw;
-
-    .post-category-wrap {
-      text-align: center;
-      margin-top: 20px;
-
-      .post-category {
-        height: 50px;
-        width: 200px;
-        overflow: hidden;
-        position: relative;
-        border-radius: 3px;
-        display: inline-block;
-
-        &::after {
-          content: "▼";
-          position: absolute;
-          right: 10px;
-          top: 50%;
-          margin-top: -9px;
-          z-index: 1;
-          text-align: center;
-          width: 20px;
-          height: 18px;
-          pointer-events: none;
-        }
-        #postCategory {
-          cursor: pointer;
-          appearance: none;
-          width: 200px;
-          height: 50px;
-          font-size: 15px;
-          font-family: $font-accent;
-          background-color: $color-primary;
-          color: $color-accent;
-          border: 3px solid $color-accent;
-          text-align-last: center;
-          position: relative;
-        }
-      }
-    }
-    .post-title-input-wrap {
-      font-family: $font-accent;
-      font-size: 28px;
-      padding: 10px;
-      margin: 20px 0;
-
-      .post-title-input {
-        outline: none;
-        border: none;
-        font-family: $font-accent;
-        font-size: 28px;
-        margin-left: 106.16px;
-        display: block;
-        width: calc(100% - 106.16px);
-        border-bottom: 3px solid $color-accent;
-      }
-
-      &::before {
-        content: "Title : ";
-        padding-right: 24px;
-        float: left;
-      }
-    }
-
-    .post-upload-button-wrap {
-      text-align: center;
-      height: 100px;
-      position: relative;
-
-      .outter {
-        display: table;
-        width: 100%;
-        height: 100%;
-        .inner {
-          vertical-align: middle;
-          display: table-cell;
-
-          .post-upload-button {
-            background-color: $color-primary;
-            border: 3px solid $color-accent;
-            font-family: $font-accent;
-            font-size: 20px;
-            width: 200px;
-            height: 50px;
-            transition: 0.2s;
-
-            &:hover {
-              background-color: $color-accent;
-              color: $color-primary;
-            }
-          }
-        }
-      }
-    }
-  }
-}
+@import "@/styles/gwahangmi/post/post-editor.scss";
 </style>
