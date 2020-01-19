c092afb582c560e4368f13f2e4b9521a239cacc9
Yunseo Hwang <yunseo.h68@gmail.com>
2020-01-19 11:29:46 +0900

    Fix : 오늘의 퀴즈 정답시 정답이라는 문구 나오도록 수정

--- src/components/Quize/todayQuize.vue
+++ src/components/Quize/todayQuize.vue
@@ -19,29 +19,46 @@
           <div class="quize-title">
             만유인력의 법칙을 발견한 과학자는 누구일까요?
           </div>
-          <div class="quize-answers">
-            <div class="quize-answer">
+          <div class="quize-pass" v-if="pass">
+            <div class="outter">
+              <div class="inner">
+                <h1 class="pass-title">
+                  정답입니다! <span v-if="getIsAuth">+5</span>
+                </h1>
+                <div class="quize-login-wrap">
+                  <a href="/login" class="quize-login" v-if="!getIsAuth">
+                    로그인하고 더 많은 거 보러 가기!
+                  </a>
+                  <a href="#" class="quize-login" v-if="getIsAuth">
+                    더 많은 거 보러 가기!
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="quize-answers" v-if="!pass">
+            <div class="quize-answer" @click="quizeAnswer(1)">
               <div class="outter">
                 <div class="inner">
                   뉴턴
                 </div>
               </div>
             </div>
-            <div class="quize-answer">
+            <div class="quize-answer" @click="quizeAnswer(2)">
               <div class="outter">
                 <div class="inner">
                   스티브잡스
                 </div>
               </div>
             </div>
-            <div class="quize-answer">
+            <div class="quize-answer" @click="quizeAnswer(3)">
               <div class="outter">
                 <div class="inner">
                   유재석
                 </div>
               </div>
             </div>
-            <div class="quize-answer">
+            <div class="quize-answer" @click="quizeAnswer(4)">
               <div class="outter">
                 <div class="inner">
                   헬렌 켈러
@@ -56,8 +73,31 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-  name: "TodayQuize"
+  name: "TodayQuize",
+  data() {
+    return {
+      rightAnswer: 1,
+      pass: false
+    };
+  },
+  computed: {
+    ...mapGetters(["getUser"]),
+    getIsAuth() {
+      return this.getUser.isAuth;
+    }
+  },
+  methods: {
+    quizeAnswer(answer) {
+      if (answer === this.rightAnswer) {
+        this.pass = true;
+      } else {
+        this.pass = false;
+      }
+    }
+  }
 };
 </script>
 
