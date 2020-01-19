59189ec55d6c5381389ce313cdfbcf5ab654a2d4
Yunseo Hwang <yunseo.h68@gmail.com>
2020-01-20 01:33:59 +0900

    Feat : 프로필 버튼 추가

--- src/components/GwahangmiHeader/GwahangmiHeader.vue
+++ src/components/GwahangmiHeader/GwahangmiHeader.vue
@@ -9,16 +9,28 @@
           과학의 아름다움을 나누는 커뮤니티
         </h2>
       </hgroup>
-      <span v-if="!getIsAuth">
-        <div class="sign-wrap">
-          <a class="sign-in" href="/login">
-            로그인
-          </a>
-          <a class="sign-up" href="/signup">
-            회원가입
-          </a>
-        </div>
-      </span>
+      <div class="sign-wrap" v-if="!getIsAuth">
+        <a class="sign-in" href="javascript:void(0)" @click="goLogin">
+          로그인
+        </a>
+        <a class="sign-up" href="javascript:void(0)" @click="goSignup">
+          회원가입
+        </a>
+      </div>
+      <div class="sign-ok" v-if="getIsAuth">
+        <a class="sign-ok-profile" href="javascript:void(0)" @click="goProfile">
+          <div class="profile-img">
+            <img
+              src="https://scontent-lax3-2.xx.fbcdn.net/v/t1.0-9/p960x960/69894151_376374273281275_8676602144360497152_o.jpg?_nc_cat=111&_nc_ohc=YR94sK98E_oAQn_OfCxwcNEXMdp15IwHkHXTHCji96X48vHBFFf2R5kfg&_nc_ht=scontent-lax3-2.xx&oh=d25ff31a79d60fd75ffa83851098f98c&oe=5E9B3008"
+              width="100%"
+              alt="profile img"
+            />
+          </div>
+          <div class="user-name">
+            {{ getUname }}
+          </div>
+        </a>
+      </div>
       <nav class="gnb-wrap">
         <div class="gnb">
           <div class="item-wrap">
@@ -46,13 +58,30 @@
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "GwahangmiHeader",
   computed: {
     ...mapGetters(["getUser"]),
     getIsAuth() {
       return this.getUser.isAuth;
+    },
+    getUname() {
+      return this.getUser.uname;
+    }
+  },
+  methods: {
+    ...mapActions(["logout"]),
+    goProfile() {
+      this.$router.push("profile");
+    },
+    goLogin() {
+      this.logout();
+      this.$router.push("login");
+    },
+    goSignup() {
+      this.logout();
+      this.$router.push("signup");
     }
   }
 };
